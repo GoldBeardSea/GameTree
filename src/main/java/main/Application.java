2e2f58ccd3b9dced5a7a347f74720d4d7232feb9
GoldBeardSea <timbusch1@gmail.com>
2018-08-06 21:26:27 -0700

    persistent login working

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -2,12 +2,42 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+//@SpringBootApplication
+//public class Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//        System.out.println("http://localhost:8080");
+//    }
+//}
+
+@Controller
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("http://localhost:8080");
+
+    }
+
+    @GetMapping("/")
+    public String homepage(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession();
+        String login = (String) session.getAttribute("login");
+        System.out.println(session.getId() + " " + login);
+        if (session.getAttribute("loggedin") == null) {
+            model.addAttribute("login", "user");
+        }
+        if (login != null) {
+            model.addAttribute("login", login);
+        }
+        return "index";
     }
 }
